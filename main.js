332bceadecf7775dd5a3d469392a454f7ded2ff5
unknown <eb30780@seeu.edu.mk>
2025-10-01 17:52:18 +0200

    Resolve merge conflict in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+
 import * as THREE from 'three';
 
-<<<<<<< HEAD
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
 
@@ -61,16 +61,13 @@ function animate() {
 
     const t = clock.getElapsedTime();
 
-   
     sphere.rotation.x += 0.01;
     sphere.rotation.y += 0.01;
     sphere.position.x = -2 + Math.sin(t) * 1.0;
 
-   
     cone.rotation.y += 0.02;
     cone.position.y = Math.sin(t * 2) * 1.0;
 
-    
     cylinder.rotation.x += 0.015;
     cylinder.rotation.y += 0.015;
     cylinder.position.z = Math.sin(t) * 1.5;
@@ -79,34 +76,4 @@ function animate() {
 }
 
 animate();
-=======
-const scene=new THREE.Scene();
-scene.background=new THREE.Color(0x202020);
 
-const camera=
-new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-camera.position.z=3;
-
-const renderer=new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry=new THREE.BoxGeometry(1,1,1);
-const material=new THREE.MeshBasicMaterial({color:0x00ff88});
-const cubeMesh=new THREE.Mesh(geometry,material);
-scene.add(cubeMesh);
-
-const light=new THREE.DirectionalLight(0xffffff,1);
-light.position.set(2,2,5);
-scene.add(light);
-
-//cubeMesh.rotation.x=10;
-
-function animate(){
-    requestAnimationFrame(animate);
-    cubeMesh.rotation.x +=0.01;
-    renderer.render(scene,camera);
-}
-
-animate()
->>>>>>> feb7abdc4b011ee5f441fb00bfb988020e0123b9
